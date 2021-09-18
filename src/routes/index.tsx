10b68d9104e30c880c229b0bcbdd3ce5fbd06a9f
RiadOliveira <riad.oliveira@hotmail.com>
2021-09-17 22:46:42 -0300

    Animations added

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,36 @@ import React from 'react';
 import { Switch, Route, useLocation } from 'react-router-dom';
 import Landing from 'pages/Landing';
 import RegisterCompany from 'pages/RegisterCompany';
+import { useTransition, animated } from 'react-spring';
 
 const Routes: React.FC = () => {
-  const { pathname } = useLocation();
+  const location = useLocation();
+
+  const transition = useTransition(location, {
+    from: { opacity: 0, width: '80vw', height: '80vh' },
+    enter: {
+      opacity: 1,
+      width: '100vw',
+      height: '100vh',
+    },
+    leave: { opacity: 0, position: 'relative' },
+
+    config: {
+      duration: 800,
+    },
+  });
 
   return (
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/register-company" component={RegisterCompany} />
-    </Switch>
+    <>
+      {transition((style, item) => (
+        <animated.div style={{ ...style, position: 'absolute' }}>
+          <Switch location={item}>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register-company" component={RegisterCompany} />
+          </Switch>
+        </animated.div>
+      ))}
+    </>
   );
 };
 
