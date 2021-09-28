53d24037426219a72058675ab55058a61350d6de
RiadOliveira <riad.oliveira@hotmail.com>
2021-09-28 11:08:01 -0300

    Toast Transition and hover

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,17 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useTransition } from 'react-spring';
 import { Container, MainText, SubText } from './styles';
 
 const Toast: React.FC = () => {
-  return (
-    <Container>
-      <MainText>
-        <p>Cadastro concluído!</p>
-      </MainText>
+  const [isVisible, setIsVisible] = useState(true);
 
-      <SubText>
-        <p>Pronto para criar produtos</p>
-      </SubText>
-    </Container>
+  const toastTransition = useTransition(isVisible, {
+    from: {
+      top: -80,
+      opacity: 0,
+    },
+    enter: {
+      top: 16,
+      opacity: 1,
+    },
+    leave: {
+      top: -80,
+      opacity: 0,
+    },
+    config: {
+      duration: 300,
+    },
+  });
+
+  return (
+    <>
+      {toastTransition((style, item) => (
+        <>
+          {item && (
+            <Container onClick={() => setIsVisible(false)} style={style}>
+              <MainText>
+                <p>Cadastro concluído!</p>
+              </MainText>
+
+              <SubText>
+                <p>Pronto para criar produtos</p>
+              </SubText>
+            </Container>
+          )}
+        </>
+      ))}
+    </>
   );
 };
 
--- src/components/Toast/styles.ts
+++ src/components/Toast/styles.ts
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
+import { animated } from 'react-spring';
 import { shade } from 'polished';
 
-export const Container = styled.button`
+export const Container = styled(animated.button)`
   border: 0;
   outline: 0;
   color: #fff;
   cursor: pointer;
 
   position: absolute;
-  top: 10px;
-  right: 10%;
+  top: 16px;
+  right: 4%;
 
   width: 310px;
   padding: 10px 0;
@@ -24,6 +25,31 @@ export const Container = styled.button`
   border-radius: 10px;
 
   line-height: 0px;
+
+  &:hover {
+    animation: shake 0.4s;
+  }
+
+  @keyframes shake {
+    0% {
+      margin-right: 1.5px;
+    }
+    20% {
+      margin-right: 0px;
+    }
+    40% {
+      margin-right: -1.5px;
+    }
+    60% {
+      margin-right: 0px;
+    }
+    80% {
+      margin-right: 1.5px;
+    }
+    100% {
+      margin-right: 0px;
+    }
+  }
 `;
 
 export const MainText = styled.section`
