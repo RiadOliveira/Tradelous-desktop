dea9a98b7ecc6d0fffaed7327d9850fca6f7ed1d
Riad Oliveira de Morais <riad.oliveira@hotmail.com>
2021-10-22 01:48:42 -0300

    UserCompanyData finished

--- src/pages/Dashboard/Profile/UserCompanyData/index.tsx
+++ src/pages/Dashboard/Profile/UserCompanyData/index.tsx
@@ -1,6 +1,53 @@
-import React from 'react';
-import { Container } from './styles';
+import React, { useEffect, useState } from 'react';
+import api from 'services/api';
 
-const UserCompany: React.FC = () => <Container />;
+import { MdDomain } from 'react-icons/md';
+import { useAuth } from 'hooks/auth';
+import { Container, CompanyIcon, CompanyImage, NoCompanyText } from './styles';
+
+interface ICompany {
+  name: string;
+  address: string;
+  logo?: string;
+}
+
+const UserCompany: React.FC = () => {
+  const { user } = useAuth();
+
+  const [company, setCompany] = useState<ICompany>({} as ICompany);
+
+  const apiStaticUrl = `${api.defaults.baseURL}/files`;
+
+  useEffect(() => {
+    if (user.companyId) {
+      api.get('/company').then(response => {
+        setCompany(response.data);
+      });
+    }
+  }, [user.companyId]);
+
+  return (
+    <Container>
+      {company.name ? (
+        <>
+          <CompanyIcon>
+            {company.logo ? (
+              <CompanyImage src={`${apiStaticUrl}/logo/${company.logo}`} />
+            ) : (
+              <MdDomain size={220} color="#1c274e" />
+            )}
+          </CompanyIcon>
+
+          <strong>{company.name}</strong>
+          <small>{company.address}</small>
+        </>
+      ) : (
+        <NoCompanyText>
+          Sua conta ainda não está associada a nenhuma empresa.
+        </NoCompanyText>
+      )}
+    </Container>
+  );
+};
 
 export default UserCompany;
--- src/pages/Dashboard/Profile/UserCompanyData/styles.ts
+++ src/pages/Dashboard/Profile/UserCompanyData/styles.ts
@@ -1,3 +1,51 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  height: 80%;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  font-family: Poppins;
+
+  strong {
+    font-size: 48px;
+    margin-top: 40px;
+  }
+
+  small {
+    font-size: 24px;
+  }
+`;
+
+export const CompanyIcon = styled.div`
+  width: 300px;
+  height: 300px;
+  background-color: #349beb;
+
+  margin-top: 4%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 3px solid ${shade(0.1, '#c4c4c4')};
+  border-radius: 50%;
+`;
+
+export const CompanyImage = styled.img`
+  width: 300px;
+  height: 300px;
+
+  border-radius: 50%;
+`;
+
+export const NoCompanyText = styled.h2`
+  width: 80%;
+  text-align: center;
+`;
